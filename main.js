4befcbd49d50f39eae8cf06ff7ecd28d520c3231
Huiqin <kathryn.h.gao@gmail.com>
2019-05-07 20:24:45 +0800

    监听键盘的上下左右移动

--- main.js
+++ main.js
@@ -124,3 +124,252 @@ function prepare_for_mobile() {
     $('.grid_cell').css('height', cell_side_length);
     $('.grid_cell').css('border-radius', 0.02 * grid_container_width);
 }
+
+//监听键盘的上下左右移动
+$(document).keydown(function(event) {
+    if ($('#score').text() == success_string) {
+        new_game();
+        return;
+    }
+    switch (event.keyCode) {
+        case 37: //left
+            event.preventDefault();
+            if (move_left()) {
+                setTimeout('generate_one_number()', 210);
+                setTimeout('is_gameover()', 300);
+            }
+            break;
+        case 38: //up
+            event.preventDefault();
+            if (move_up()) {
+                setTimeout('generate_one_number()', 210);
+                setTimeout('is_gameover()', 300);
+            }
+            break;
+        case 39: //right
+            event.preventDefault();
+            if (move_right()) {
+                setTimeout('generate_one_number()', 210);
+                setTimeout('is_gameover()', 300);
+            }
+            break;
+        case 40: //down
+            event.preventDefault();
+            if (move_down()) {
+                setTimeout('generate_one_number()', 210);
+                setTimeout('is_gameover()', 300);
+            }
+            break;
+        default:
+            break;
+    }
+});
+
+//监听移动设备的触摸开始
+document.addEventListener('touchstart', function(event) {
+    startx = event.touches[0].pageX;
+    starty = event.touches[0].pageY;
+});
+
+//监听移动设备的触摸移动
+document.addEventListener('touchmove', function(event) {
+    event.preventDefault();
+});
+
+//监听移动设备的触摸结束
+document.addEventListener('touchend', function(event) {
+    endx = event.changedTouches[0].pageX;
+    endy = event.changedTouches[0].pageY;
+
+    var deltax = endx - startx;
+    var deltay = endy - starty;
+    if (Math.abs(deltax) < 0.3 * document_width && Math.abs(deltay) < 0.3 * document_width) {
+        return;
+    }
+    if ($('#score').text() == success_string) {
+        new_game();
+        return;
+    }
+    //x
+    if (Math.abs(deltax) >= Math.abs(deltay)) {
+        if (deltax > 0) {
+            //move right
+            if (move_right()) {
+                setTimeout('generate_one_number()', 210);
+                setTimeout('is_gameover()', 300);
+            }
+        } else {
+            //move left
+            if (move_left()) {
+                setTimeout('generate_one_number()', 210);
+                setTimeout('is_gameover()', 300);
+            }
+        }
+    } else {    //y
+        if (deltay > 0) {
+            //move down
+            if (move_down()) {
+                setTimeout('generate_one_number()', 210);
+                setTimeout('is_gameover()', 300);
+            }
+        } else {
+            //move up
+            if (move_up()) {
+                setTimeout('generate_one_number()', 210);
+                setTimeout('is_gameover()', 300);
+            }
+        }
+    }
+});
+
+//向左移动
+function move_left() {
+    if (!can_move_left(board)) {
+        return false;
+    }
+    //move left
+    for (var i = 0; i < 4; i++) {
+        for (var j = 1; j < 4; j++) {
+            if (board[i][j] != 0) {
+                for (var k = 0; k < j; k++) {
+                    if (board[i][k] == 0 && no_block_horizontal(i, k, j, board)) {
+                        show_move_animation(i, j, i, k);
+                        board[i][k] = board[i][j];
+                        board[i][j] = 0;
+                        break;
+                    } else if (board[i][k] == board[i][j] && no_block_horizontal(i, k, j, board) && !has_conflicted[i][k]) {
+                        show_move_animation(i, j, i, k);
+                        board[i][k] += board[i][j]
+                        board[i][j] = 0;
+                        //add score
+                        score += board[i][k];
+                        update_score(score);
+                        has_conflicted[i][k] = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    setTimeout('update_board_view()', 200);
+    return true;
+}
+
+//向右移动
+function move_right() {
+    if (!can_move_right(board)) {
+        return false;
+    }
+    //move right
+    for (var i = 0; i < 4; i++) {
+        for (var j = 2; j >= 0; j--) {
+            if (board[i][j] != 0) {
+                for (var k = 3; k > j; k--) {
+                    if (board[i][k] == 0 && no_block_horizontal(i, j, k, board)) {
+                        show_move_animation(i, j, i, k);
+                        board[i][k] = board[i][j];
+                        board[i][j] = 0;
+                        break;
+                    } else if (board[i][k] == board[i][j] && no_block_horizontal(i, j, k, board) && !has_conflicted[i][k]) {
+                        show_move_animation(i, j, i, k);
+                        board[i][k] += board[i][j];
+                        board[i][j] = 0;
+                        //add score
+                        score += board[i][k];
+                        update_score(score);
+                        has_conflicted[i][k] = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    setTimeout('update_board_view()', 200);
+    return true;
+}
+
+//向上移动
+function move_up() {
+    if (!can_move_up(board)) {
+        return false;
+    }
+    //move up
+    for (var j = 0; j < 4; j++) {
+        for (var i = 1; i < 4; i++) {
+            if (board[i][j] != 0) {
+                for (var k = 0; k < i; k++) {
+                    if (board[k][j] == 0 && no_block_vertical(j, k, i, board)) {
+                        show_move_animation(i, j, k, j);
+                        board[k][j] = board[i][j];
+                        board[i][j] = 0;
+                        break;
+                    } else if (board[k][j] == board[i][j] && no_block_vertical(j, k, i, board) && !has_conflicted[k][j]) {
+                        show_move_animation(i, j, k, j);
+                        board[k][j] += board[i][j];
+                        board[i][j] = 0;
+                        //add score
+                        score += board[k][j];
+                        update_score(score);
+                        has_conflicted[k][j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    setTimeout('update_board_view()', 200);
+    return true;
+}
+
+//向下移动
+function move_down() {
+    if (!can_move_down(board)) {
+        return false;
+    }
+    //move down
+    for (var j = 0; j < 4; j++) {
+        for (var i = 2; i >= 0; i--) {
+            if (board[i][j] != 0) {
+                for (var k = 3; k > i; k--) {
+                    if (board[k][j] == 0 && no_block_vertical(j, i, k, board)) {
+                        show_move_animation(i, j, k, j);
+                        board[k][j] = board[i][j];
+                        board[i][j] = 0;
+                        break;
+                    } else if (board[k][j] == board[i][j] && no_block_vertical(j, i, k, board) && !has_conflicted[k][j]) {
+                        show_move_animation(i, j, k, j);
+                        board[k][j] += board[i][j];
+                        board[i][j] = 0;
+                        //add score
+                        score += board[k][j];
+                        update_score(score);
+                        has_conflicted[k][j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    setTimeout('update_board_view()', 200);
+    return true;
+}
+
+//判断游戏成功或失败
+function is_gameover() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (board[i][j] == 2048) {
+                update_score(success_string);
+                return;
+            }
+        }
+    }
+    if (nospace(board) && nomove(board)) {
+        gameover();
+    }
+}
+
+//游戏结束时更新游戏失败的文字
+function gameover() {
+    update_score(gameover_string);
+}
